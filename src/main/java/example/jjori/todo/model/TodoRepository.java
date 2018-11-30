e29fd6c32298c6b86baca1d09b8ab391c897bb90
jg.jo <jg.jo@widerplanet.com>
2018-11-30 15:10:55 +0900

    추가. id로 todo 정보를 가져오기

--- src/main/java/example/jjori/todo/model/TodoRepository.java
+++ src/main/java/example/jjori/todo/model/TodoRepository.java
@@ -2,5 +2,9 @@ package example.jjori.todo.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+    @Override
+    Optional<Todo> findById(Long id);
 }
